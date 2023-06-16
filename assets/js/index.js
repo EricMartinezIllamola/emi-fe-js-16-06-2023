fba9c0c7a8a98b08f99e7527a0fc1647eda5d919
unknown <eric77711@gmail.com>
2023-06-16 19:41:45 +0200

    ejemplo CV

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+fetch("./assets/json/esp.json")
+    .then(response => response.json())
+    .then(respuesta => {
+        $("#Home").append($("<p></p>").text(respuesta.nombre));
+        $("#Home").append($("<p></p>").text(respuesta.edad));
+        $("#Home").append($("<p></p>").text(respuesta.email));
+    });
