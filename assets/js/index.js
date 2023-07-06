15c206ef3e50ae69ab2b8ed935ffb0c6615050f3
unknown <eric77711@gmail.com>
2023-07-06 02:46:57 +0200

    nav_bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,18 @@
 //         $("#Home").append($("<p></p>").text(respuesta.edad));
 //         $("#Home").append($("<p></p>").text(respuesta.email));
 //     });
+
+
+
+const btn_menu = document.querySelector(".btn_menu");
+const ul_nav_menu = document.querySelector(".ul_nav_menu");
+
+btn_menu.addEventListener("click", () => {
+    ul_nav_menu.classList.toggle("mostrar_menu");
+});
+
+const btn_start = document.querySelector(".btn_start");
+
+btn_start.addEventListener("click", () => {
+    location.href='#Objetivos'
+});
