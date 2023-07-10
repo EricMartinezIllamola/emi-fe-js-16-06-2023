9fad0d40f516e3d3ce4250bf1aa0e6635c05296e
unknown <eric77711@gmail.com>
2023-07-10 03:57:45 +0200

    hardskills + card

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@
 //     });
 
 
-
+//abrir-cerrar boton menu pantalla movil
 const btn_menu = document.querySelector(".btn_menu");
 const ul_nav_menu = document.querySelector(".ul_nav_menu");
 
@@ -19,4 +19,14 @@ const btn_start = document.querySelector(".btn_start");
 
 btn_start.addEventListener("click", () => {
     location.href='#Objetivos'
+});
+
+const perfil_card = document.querySelector(".perfil_card");
+const card_backside = document.querySelector(".card_backside");
+const card_page = document.querySelector(".card_page");
+
+perfil_card.addEventListener("click", () => {
+    perfil_card.classList.toggle("perfil_card_invi");
+    card_backside.classList.toggle("card_backside_show");
+    card_page.classList.toggle("card_page_show");
 });
